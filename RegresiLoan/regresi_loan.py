06b0f91ceaf7541cd4d15cd5dc98c1e95b1d9b42
Dwi Hartanto <89287777+dwixhartanto@users.noreply.github.com>
2025-06-18 00:02:07 +0700

    Update regresi_loan.py

--- RegresiLoan/regresi_loan.py
+++ RegresiLoan/regresi_loan.py
@@ -8,10 +8,10 @@ st.title("Aplikasi Prediksi Income Pinjaman V2")
 # --- Muat Model ---
 # Pastikan file model Anda (regresi_loan.pkl) ada di direktori yang sama dengan aplikasi Streamlit Anda
 try:
-    model = joblib.load('regresi_loan.pkl')
-    st.success("Model 'regresi_loan.pkl' berhasil dimuat!")
+    model = joblib.load('reg_joblib')
+    st.success("Model 'reg_joblib' berhasil dimuat!")
 except FileNotFoundError:
-    st.error("Error: Model 'regresi_loan.pkl' tidak ditemukan. Pastikan file model ada di direktori yang benar.")
+    st.error("Error: Model 'reg_joblib' tidak ditemukan. Pastikan file model ada di direktori yang benar.")
     st.stop() # Hentikan aplikasi jika model tidak ditemukan
 except Exception as e:
     st.error(f"Terjadi kesalahan saat memuat model: {e}")
